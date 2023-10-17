93e732524a92df4f1d43fa752e2b63a21b7f4fcb
nkartikaxioned <kartikn@axioned.com>
2023-10-17 12:58:44 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,45 @@
 const button = document.querySelectorAll('.cross'),
   panel = document.querySelectorAll('.panel'),
-  accordion = document.querySelectorAll('.accordion');
+  container = document.querySelectorAll('.contain');
+console.log(container.length);
 
-accordion.forEach((accord, index) => {
+container.forEach((element, index) => {
 
-  button.forEach(btn => {
-    btn.classList.add('active');
+  const prevIndex = (index - 1 + container.length) % container.length;
+  const nextIndex = (index + 1) % container.length;
+
+  element.addEventListener('click', () => {
+    removeActive();
+    element.classList.toggle('active-container');
+    button[index].classList.toggle('cross-active');
+    panel[index].classList.toggle('active-panel');
+
+    container[prevIndex].classList.remove('active-container')
+    container[nextIndex].classList.remove('active-container')
+    button[prevIndex].classList.remove('cross-active')
+    button[nextIndex].classList.remove('cross-active')
+    panel[prevIndex].classList.remove('active-panel')
+    panel[nextIndex].classList.remove('active-panel')
   })
-  accord.addEventListener('click', () => {
-    removeactivep()
-    removeActiveAccordion();
-    // accord.classList.add('active-accordian');
-    accord.classList.toggle('active-accordian')
-    button[index].classList.toggle('active');
-    panel[index].classList.toggle('activepanel')
-    panel[index].classList.style.maxheight = '40px';
-
-    switch (index) {
-      case 0:
-        button[button.length - 1].classList.add('active');
-        panel[button.length - 1].classList.remove('activepanel')
-        button[index + 1].classList.add('active');
-        panel[index + 1].classList.remove('activepanel')
-        accordion[button.length - 1].classList.remove('active-accordian');
-        accordion[index + 1].classList.remove('active-accordian');
-        break;
-      case 1:
-        button[index - 1].classList.add('active');
-        panel[index - 1].classList.remove('activepanel')
-        button[index + 1].classList.add('active');
-        panel[index + 1].classList.remove('activepanel')
-        accordion[index - 1].classList.remove('active-accordian');
-        accordion[index + 1].classList.remove('active-accordian');
-        break;
-      case 2:
-        button[index - 1].classList.add('active');
-        panel[index - 1].classList.remove('activepanel')
-        button[index - 2].classList.add('active');
-        panel[index - 2].classList.remove('activepanel')
-        accordion[index - 1].classList.remove('active-accordian');
-        accordion[index -2].classList.remove('active-accordian');
-        break;
-    }
-  })
-
 })
-function removeActiveAccordion() {
-  accordion.forEach(accord => {
-    if(accord.querySelector('active-accordian')){
 
-      accord.classList.remove('active-accordian')
-    }
-
-  })
-}
-
-function removeactivep() {
-  panel.forEach(p => {
-    if (p.querySelector("activepanel")) {
-      p.classList.remove('activepanel');
+function removeActive() {
+  container.forEach(contain => {
+    if(contain.querySelector('active-container')) {
+      contain.classList.remove('active-container');
     }
   })
+  button.forEach(elem => {
+    if(elem.querySelector('cross-active')) {
+      elem.classList.remove('cross-active');
+    }
+  })
+  panel.forEach(pan => {
+    if(pan.querySelector('active-panel')) {
+      pan.classList.remove('active-panel');
+    }
+  })
 }
-
-
-
-
 
 
 
